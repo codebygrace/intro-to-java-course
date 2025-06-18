9b61f12289741673ea2fadb0d19020151598c2e1
codebygrace <gracekayode@hotmail.com>
2025-06-18 17:27:36 +0100

    Completed Classes & Objects - Part 2

--- exercises/object-oriented-programming/cars/src/main/java/com/cbfacademy/cars/App.java
+++ exercises/object-oriented-programming/cars/src/main/java/com/cbfacademy/cars/App.java
@@ -3,5 +3,10 @@ package com.cbfacademy.cars;
 public class App {
     public static void main(String[] args) {
         // TODO: Display details of all cars in showroom
+        
+        Showroom showroom1 = new Showroom();
+
+        System.out.println(showroom1.getCars());
+    
     }
 }
---
+++ exercises/object-oriented-programming/cars/src/main/java/com/cbfacademy/cars/Showroom.java
@@ -0,0 +1,14 @@
+package com.cbfacademy.cars;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Showroom {
+    public List<Car> getCars() {
+        List<Car> cars = new ArrayList<>();
+        cars.add(new Car("Volvo", "V40", "blue", 2012));
+        cars.add(new Car("Porsche", "Panamera", "red", 2009));
+        cars.add(new Car("Audi", "A3", "grey", 2018));
+        return cars;
+    }
+}
